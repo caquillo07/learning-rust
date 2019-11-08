70671d0a3f8e597720f8c3545b1d303ae68ba663
Hector Mejia <caquillo722@gmail.com>
2019-11-07 21:49:24 -0600

    finished ch13 section 1

--- ch12_minigrep/src/main.rs
+++ ch12_minigrep/src/main.rs
@@ -1,38 +1,19 @@
 use std::env;
-use std::fs;
 use std::process;
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config , &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        return Ok(Config {query, filename});
-    }
-}
+use ch12_minigrep::Config;
 
 fn main() {
-    println!("Hello, world!");
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Error parsing arguments: {}", err);
+        eprintln!("Error parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
+    println!("Searching for {} in file {}", config.query, config.filename);
 
-    let contents = fs::read_to_string(config.filename).expect("Error reading file");
-    println!("With text:\n{}", contents);
-
+    if let Err(err) = ch12_minigrep::run(config) {
+        eprintln!("Application error: {}", err);
+        process::exit(1);
+    }
 }
-
